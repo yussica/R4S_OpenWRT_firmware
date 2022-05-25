fc0f87ee3c25b56d06f5a2b988c22031954b8bcb
yussica <zhuyech@gmail.com>
2022-05-25 11:17:19 +0800

    openwrt-subconverter

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git subconverter https://github.com/tindy2013/openwrt-subconverter' >>feeds.conf.default
